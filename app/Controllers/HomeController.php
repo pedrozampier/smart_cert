d241b88015c5ea8545b3c035778aeb87ad09581b
pedrozampier <pedrootaviopz@gmail.com>
2025-05-17 21:12:27 -0300

    feat: Add admin and user dashboard methods to HomeController

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -9,6 +9,18 @@ class HomeController extends Controller
     public function index(): void
     {
         $title = 'Home Page';
+        $this->render('login/index', compact('title'));
+    }
+
+    public function adminDashboard(): void
+    {
+        $title = 'Admin Dashboard';
+        $this->render('admin/home/index', compact('title'));
+    }
+
+    public function userDashboard(): void
+    {
+        $title = 'User Dashboard';
         $this->render('home/index', compact('title'));
     }
 }
