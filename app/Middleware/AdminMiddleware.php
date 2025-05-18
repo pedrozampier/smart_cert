8057f3fcf64bdbddcab6b7139e2a51e26ccaa502
pedrozampier <pedrootaviopz@gmail.com>
2025-05-17 21:12:50 -0300

    feat: Add AdminMiddleware to restrict access to admin-only pages

---
+++ app/Middleware/AdminMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Middleware;
+
+use Core\Http\Middleware\Middleware;
+use Core\Http\Request;
+use Lib\Authentication\Auth;
+use Lib\FlashMessage;
+
+class AdminMiddleware implements Middleware
+{
+    public function handle(Request $request): void
+    {
+        if (!Auth::check() || !Auth::user()->admin) {
+            FlashMessage::danger('Você deve ser administrador para acessar essa página');
+            $this->redirectTo('/user/dashboard');
+        }
+    }
+
+    private function redirectTo(string $location): void
+    {
+        header('Location: ' . $location);
+        exit;
+    }
+}
